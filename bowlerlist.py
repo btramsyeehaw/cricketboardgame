1e9f6fa1d0430bb67bf1d7d218ce70f526639f01
btramsyeehaw <51712687+btramsyeehaw@users.noreply.github.com>
2022-10-10 22:16:33 +0200

    added team column, column labels

--- bowlerlist.py
+++ bowlerlist.py
@@ -2,9 +2,19 @@ import pandas as pd
 import numpy as np
 tournament_file = pd.read_csv("2022TheHundred.csv")
 
-bowlerlist = [*set(tournament_file['bowler'].to_list())]
+longbowlerlist = tournament_file['bowler'].to_list()
+bowlerlist = [*set(longbowlerlist)]
 bowlerlist.remove('bowler')
 bowlerlist.sort()
-bowlerlistdf = pd.DataFrame(bowlerlist)
+bowlerlistdf = pd.DataFrame()
+bowlerteamlist = []
+
+for x in bowlerlist:
+	for i in range(len(longbowlerlist)):
+		if longbowlerlist[i]==x:
+			bowlerteamlist.append(tournament_file["bowling_team"].to_list()[i])
+			break
+bowlerlistdf["bowler"] = bowlerlist
+bowlerlistdf["team"] = bowlerteamlist
 bowlerlistdf.to_csv("2022HundoBowlers.csv")
 print(len(bowlerlistdf),"bowlers added to list")
